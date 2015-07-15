18a22127d42f9b84378fbb5846b7a7434bb12fb5
Mohamed Zougam <mohamed.zougam.a@gmail.com>
2015-07-15 17:03:21 +0100

    Updated scripts

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -3,3 +3,10 @@
  */
 
 'use strict';
+
+if('querySelector' in document
+     && 'localStorage' in window
+     && 'addEventListener' in window) {
+     // bootstrap the javascript application
+ 		console.log('HTML 5 Browser!')
+     }
