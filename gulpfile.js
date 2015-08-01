2c36e987c4f94fd81a484c0fedf72484f71b10b0
q <mohamed.zougam.a@gmail.com>
2015-08-01 20:29:47 +0400

    Added bi-directional support

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,9 @@ var config = {
 		},
 		styles: {
 			fabricator: 'src/assets/fabricator/styles/fabricator.scss',
-			toolkit: 'src/assets/toolkit/styles/toolkit.scss'
+			toolkit: 'src/assets/toolkit/styles/toolkit.scss',
+			ltr: 'src/assets/toolkit/styles/ltr.scss',
+			rtl: 'src/assets/toolkit/styles/rtl.scss'
 		},
 		images: 'src/assets/toolkit/images/**/*',
 		views: 'src/toolkit/views/*.html'
@@ -67,7 +69,25 @@ gulp.task('styles:toolkit', function () {
 		.pipe(gulpif(config.dev, reload({stream:true})));
 });
 
-gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
+gulp.task('styles:ltr', function () {
+	gulp.src(config.src.styles.ltr)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefix('last 1 version'))
+		.pipe(gulpif(!config.dev, csso()))
+		.pipe(gulp.dest(config.dest + '/assets/toolkit/styles'))
+		.pipe(gulpif(config.dev, reload({stream:true})));
+});
+
+gulp.task('styles:rtl', function () {
+	gulp.src(config.src.styles.rtl)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefix('last 1 version'))
+		.pipe(gulpif(!config.dev, csso()))
+		.pipe(gulp.dest(config.dest + '/assets/toolkit/styles'))
+		.pipe(gulpif(config.dev, reload({stream:true})));
+});
+
+gulp.task('styles', ['styles:fabricator', 'styles:toolkit', 'styles:ltr', 'styles:rtl']);
 
 
 // scripts
@@ -147,6 +167,12 @@ gulp.task('serve', function () {
 
 	gulp.task('styles:toolkit:watch', ['styles:toolkit']);
 	gulp.watch('src/assets/toolkit/styles/**/*.scss', ['styles:toolkit:watch']);
+
+	gulp.task('styles:ltr:watch', ['styles:ltr']);
+	gulp.watch('src/assets/toolkit/styles/**/*.scss', ['styles:ltr:watch']);
+
+	gulp.task('styles:rtl:watch', ['styles:rtl']);
+	gulp.watch('src/assets/toolkit/styles/**/*.scss', ['styles:rtl:watch']);
 
 	gulp.task('scripts:watch', ['scripts'], reload);
 	gulp.watch('src/assets/{fabricator,toolkit}/scripts/**/*.js', ['scripts:watch']).on('change', webpackCache);
