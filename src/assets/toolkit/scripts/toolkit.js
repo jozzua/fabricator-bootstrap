a4e9a49d0e335305e653d90f004cd6fa76a8511c
Ely Apao <jozzua@gmail.com>
2017-01-31 20:44:16 +0800

    Setup fabricator with Bootstrap

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,12 +1,3 @@
 /**
  * Toolkit JavaScript
  */
-
-'use strict';
-
-	// plugins
-	require("script!./plugins/jquery-1.11.3");
-	require('./plugins/bootstrap');
-
-	// modules
-	require('./modules/globals');
